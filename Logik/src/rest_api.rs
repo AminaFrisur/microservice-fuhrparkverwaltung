7b3fd6a432b0b09511daa200cfd54925ce0f4b3e
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-09-21 15:07:10 +0200

    added regular expression filtering for routing. Tim.

--- Logik/src/rest_api.rs
+++ Logik/src/rest_api.rs
@@ -2,11 +2,10 @@
   use regex::Regex;
 
   pub fn choose_method_delete(http_method: &str, route: &str, body: &str) -> &'static str {
+        let re = Regex::new("/deleteVehicle").unwrap();
+        let filtered_route: &str = &regex_route(re, route)[..];
 
-        println!("{}", route);
-        println!("{}", body);
-
-        match route {
+        match filtered_route {
                 "/deleteVehicle" => return delete_vehicle(),
                 &_ => return "route not found",
         };
@@ -14,9 +13,10 @@
   }
 
   pub fn choose_method_post(http_method: &str, route: &str, body: &str) -> &'static str {
-          println!("{}", route);
-          println!("{}", body);
-          match route {
+
+          let re = Regex::new("/addVehicle|/updateVehicle").unwrap();
+          let filtered_route: &str = &regex_route(re, route)[..];
+          match filtered_route {
                          "/addVehicle" => return add_vehicle(),
                          "/updateVehicle" => return update_vehicle(),
                           &_ => return "route not found",
@@ -24,53 +24,54 @@
   }
 
   pub fn choose_method_get(http_method: &str, route: &str, body: &str) -> &'static str {
-          println!("{}", route);
-          println!("{}", body);
-          match route {
-                         "/getVehicle/" => return get_vehicle(),
+          let re = Regex::new(r"/getVehicle\?|/getVehicles").unwrap();
+          let filtered_route: &str = &regex_route(re, route)[..];
+          match filtered_route {
+                         "/getVehicle?" => return get_vehicle(),
                          "/getVehicles" => return get_vehicles(),
                          &_ => return "route not found",
                        };
   }
 
   pub fn choose_method_put(http_method: &str, route: &str, body: &str) -> &'static str {
-          println!("{}", route);
-          println!("{}", body);
           match route {
                            &_ => return "route not found",
                        };
   }
 
   pub fn choose_method_patch(http_method: &str, route: &str, body: &str) -> &'static str {
-          println!("{}", route);
-          println!("{}", body);
           match route {
                          &_ => return "route not found",
                       };
   }
 
+
+  pub fn regex_route(re: Regex, route: &str) -> String {
+        if re.is_match(route) {
+            let cap = re.captures(route).unwrap();
+            return (&cap[0]).to_string();
+        } else {
+            return "".to_string();
+        }
+  }
+
   pub fn add_vehicle() -> &'static str {
-      println!("addVehicle");
       return "ERFOLG mit add_vehicle";
   }
 
   pub fn get_vehicle() -> &'static str {
-      println!("getVehicle");
       return "ERFOLG mit get_vehicle";
   }
 
   pub fn get_vehicles() -> &'static str {
-        println!("getVehicles");
         return "ERFOLG mit get_vehicles";
     }
 
   pub fn update_vehicle() -> &'static str {
-      println!("updateVehicle");
       return "ERFOLG mit update_vehicle";
   }
 
   pub fn delete_vehicle() -> &'static str {
-      println!("deleteVehicle");
       return "ERFOLG mit delete_vehicle";
   }
 
