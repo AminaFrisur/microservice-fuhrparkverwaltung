f8ee71137c34ab35a8fc3a0c91a3b24bff746481
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-06 23:35:05 +0200

    tried to implement update method. Tim.

--- Rest_Api/src/main.rs
+++ Rest_Api/src/main.rs
@@ -5,6 +5,7 @@ use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, 
 use nickel::status::StatusCode;
 use std::str::FromStr;
 use itertools::Itertools;
+use std::io::Read;
 
 // TODO: Fuege Standort noch hinzu
 // Buchungsservices orientieren sich nach bestimmten Standort -> deshalb dies hinzufügen
@@ -100,13 +101,49 @@ fn main()  {
 
     });
 
-    router.post("/updateVehicle/:id", middleware! { |request, response|
+    router.post("/updateVehicle", middleware! { |request, mut response|
 
-        format!("Hello from POST /users/new")
+        let body = request.json_as::<Fahrzeug>().unwrap();
 
+        println!("{}", body);
+
+        // check parameter:
+        if request.param("id").is_none() || request.param("marke").is_none() ||
+           request.param("model").is_none() || request.param("leistung").is_none() {
+            return response.send(format!("parameters are missing"));
+        }
+
+        let id: i32 = FromStr::from_str(request.param("id").unwrap()).unwrap();
+        let marke = request.param("marke").unwrap();
+        let model = request.param("model").unwrap();
+        let leistung: i32 = FromStr::from_str(request.param("leistung").unwrap()).unwrap();
+
+        let mut client = match Client::connect("host=database port=5432 user=postgres password=test", NoTls) {
+            Ok(conn) => conn,
+            Err(e) => {
+                response.set(StatusCode::InternalServerError);
+                return response.send(format!("{}", e));
+            }
+        };
+
+        let query_result = match client.execute("UPDATE fahrzeuge (marke, model, leistung) VALUES ($1, $2, $3) WHERE id = $4", &[&marke, &model, &leistung, &id]) {
+            Ok(res) => res,
+            Err(e) => {
+                response.set(StatusCode::InternalServerError);
+                return response.send(format!("{}", e));
+            }
+        };
+
+        return response.send(format!("Updated successfull! {}", query_result));
     });
 
     router.delete("/deleteVehicle/:id", middleware! { |request, response|
+
+        format!("Hello from DELETE /users/:id")
+
+    });
+
+    router.delete("/addVehicle", middleware! { |request, response|
 
         format!("Hello from DELETE /users/:id")
 
@@ -117,3 +154,10 @@ fn main()  {
     server.listen("0.0.0.0:3030").unwrap();
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Fahrzeug {
+    id: i32,
+    marke: String,
+    model: String,
+    leistung: i32
+}
