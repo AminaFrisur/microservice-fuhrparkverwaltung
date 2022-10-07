f0cf7810f2c7eb03a30e9f21a35146d475d96917
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-07 03:46:59 +0200

    completely refactored main.rs. Removed all error handling. Now only using unwrap. Tim.

--- Rest_Api/src/main.rs
+++ Rest_Api/src/main.rs
@@ -3,17 +3,12 @@
 
 #[macro_use] extern crate rocket;
 use postgres::{Client, NoTls};
-use std::str::FromStr;
-use itertools::Itertools;
-use std::io::Read;
-use rocket::response::content;
 use rocket_contrib::json::Json;
 use serde::{Serialize, Deserialize};
 
-// TODO: set HTTP Statuscode for Errors
+// TODO: Implement Error Handling
 
-// TODO: Fuege Standort noch hinzu
-// Buchungsservices orientieren sich nach bestimmten Standort -> deshalb dies hinzufügen
+// TODO: Fuege Standort noch hinzu -> Buchungsservices orientieren sich nach bestimmten Standort -> deshalb dies hinzufügen
 
 // TODO: Authentifizierung innerhalb der MS Architektur noch festlegen
 
@@ -41,37 +36,23 @@ fn create_fahrzeug_list(query_result: Vec<postgres::row::Row>) -> Vec<Fahrzeug> 
 #[get("/getVehicle/<id>")]
 fn get_vehicle(id: i32) -> Json<Vec<Fahrzeug>> {
 
-    let mut vec_error : Vec<Fahrzeug> = Vec::new();
-    let fahrzeug = Fahrzeug {
-        id: 1,
-        marke: String::from(""),
-        model: String::from(""),
-        leistung: 1,
-    };
-    vec_error.push(fahrzeug);
-
-    let mut client = match Client::connect("host=database port=5432 user=postgres password=test", NoTls) {
-        Ok(conn) => conn,
-        Err(e) => {
-            let error_json = format!("{{ 'ERROR':{} }}", e);
-            return Json(vec_error);
-        }
-    };
-
-    let query_result = match client.query("SELECT id, marke, model, leistung FROM fahrzeuge WHERE id=$1",  &[&id]) {
-        Ok(res) => res,
-        Err(e) => {
-            let error_json = format!("{{ 'ERROR':{} }}", e);
-            return Json(vec_error);
-        }
-    };
-
+    let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
+    let query_result = client.query("SELECT id, marke, model, leistung FROM fahrzeuge WHERE id=$1",  &[&id]).unwrap();
     return Json(create_fahrzeug_list(query_result));
+
+}
+
+#[get("/getVehicles")]
+fn get_vehicles() -> Json<Vec<Fahrzeug>> {
+
+    let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
+    let query_result = client.query("SELECT id, marke, model, leistung FROM fahrzeuge",  &[]).unwrap();
+    return Json(create_fahrzeug_list(query_result));
+
 }
 
 fn main()  {
-    rocket::ignite().mount("/", routes![get_vehicle]).launch();
-
+    rocket::ignite().mount("/", routes![get_vehicle, get_vehicles]).launch();
 }
 
 #[derive(Serialize, Deserialize, Debug)]
