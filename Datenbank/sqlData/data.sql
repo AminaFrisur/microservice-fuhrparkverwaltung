f4d7167c665f7ba004d5f1a6d42293cb44264d63
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-07 18:11:25 +0200

    fixed create table statement for data.sql. Now serial sequence for primary key works correctly. Tim.

--- Datenbank/sqlData/data.sql
+++ Datenbank/sqlData/data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS fahrzeuge (
   model varchar(255) NOT NULL,
-  id SERIAL PRIMARY KEY NOT NULL ,
+  id SERIAL PRIMARY KEY,
   marke varchar(255) NOT NULL,
   leistung int DEFAULT NULL);
 
-INSERT INTO fahrzeuge VALUES ('m3',1,'BMW',450),('RS6',2,'Audi',550),('Model S',3,'Tesla',550);
+INSERT INTO fahrzeuge(model, marke, leistung) VALUES ('m3','BMW',450),('RS6','Audi',550),('Model S','Tesla',550);
