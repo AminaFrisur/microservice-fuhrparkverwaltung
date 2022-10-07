f98db1c2a714fed7d0b35e0823d684c79b5450c3
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-07 02:47:01 +0200

    completely changed to rocket. Tim.

--- Rest_Api/src/main.rs
+++ Rest_Api/src/main.rs
@@ -6,6 +6,11 @@ use postgres::{Client, NoTls};
 use std::str::FromStr;
 use itertools::Itertools;
 use std::io::Read;
+use rocket::response::content;
+use rocket_contrib::json::Json;
+use serde::{Serialize, Deserialize};
+
+// TODO: set HTTP Statuscode for Errors
 
 // TODO: Fuege Standort noch hinzu
 // Buchungsservices orientieren sich nach bestimmten Standort -> deshalb dies hinzufügen
@@ -14,14 +19,65 @@ use std::io::Read;
 
 // TODO: Rust nightly -> Wirklich empfehlenswert fuer production? -> gibt es auch eine Alternative um rocket zu kompelieren ?
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+fn create_fahrzeug_list(query_result: Vec<postgres::row::Row>) -> Vec<Fahrzeug> {
+
+    let mut vec_fahrzeuge : Vec<Fahrzeug> = Vec::new();
+
+    for row in query_result {
+
+        let fahrzeug = Fahrzeug {
+            id: row.get(0),
+            marke: row.get(1),
+            model: row.get(2),
+            leistung: row.get(3),
+        };
+        vec_fahrzeuge.push(fahrzeug);
+    }
+    return vec_fahrzeuge;
+}
+
+
+
+#[get("/getVehicle/<id>")]
+fn get_vehicle(id: i32) -> Json<Vec<Fahrzeug>> {
+
+    let mut vec_error : Vec<Fahrzeug> = Vec::new();
+    let fahrzeug = Fahrzeug {
+        id: 1,
+        marke: String::from(""),
+        model: String::from(""),
+        leistung: 1,
+    };
+    vec_error.push(fahrzeug);
+
+    let mut client = match Client::connect("host=database port=5432 user=postgres password=test", NoTls) {
+        Ok(conn) => conn,
+        Err(e) => {
+            let error_json = format!("{{ 'ERROR':{} }}", e);
+            return Json(vec_error);
+        }
+    };
+
+    let query_result = match client.query("SELECT id, marke, model, leistung FROM fahrzeuge WHERE id=$1",  &[&id]) {
+        Ok(res) => res,
+        Err(e) => {
+            let error_json = format!("{{ 'ERROR':{} }}", e);
+            return Json(vec_error);
+        }
+    };
+
+    return Json(create_fahrzeug_list(query_result));
 }
 
 fn main()  {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![get_vehicle]).launch();
 
 }
 
-
+#[derive(Serialize, Deserialize, Debug)]
+struct Fahrzeug {
+    id: i32,
+    marke: String,
+    model: String,
+    leistung: i32,
+}
